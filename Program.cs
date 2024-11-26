03db20783ce55a3d929ce696bfb1c3e8858fdfd4
everlast1ngw1nter <at07012004@gmail.com>
2024-11-26 16:31:45 +0500

    DONE 3

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
+using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
 using Serilog;
 using Serilog.Events;
 
 
 
 
-string logFilePath = Path.Combine(".logs", $"start-host-log-{DateTime.Now:yyyy-MM-dd}.txt");
-Log.Logger = new LoggerConfiguration()
-        .WriteTo.File(logFilePath, LogEventLevel.Error) 
-        .WriteTo.Console()
-        .CreateLogger();
-Log.Information($"Starting application. Logging to: {logFilePath}");
+// string logFilePath = Path.Combine(".logs", $"start-host-log-{DateTime.Now:yyyy-MM-dd}.txt");
+// Log.Logger = new LoggerConfiguration()
+//
+//         .CreateLogger();
+//
+// Log.Information($"Starting application. Logging to: {logFilePath}");
 
 try {  
     var builder = WebApplication.CreateBuilder(args);
@@ -18,12 +23,31 @@ try {
     builder.Services.AddRazorPages();
     builder.Host.UseSerilog();
 
-
+    Log.Logger = new LoggerConfiguration()
+        // .WriteTo.File(logFilePath, LogEventLevel.Error) 
+        .WriteTo.Console()
+        .Enrich.FromLogContext()
+        .WriteTo.Elasticsearch(new []{ new Uri("http://localhost:9200")}, opts =>
+        {
+            opts.DataStream = new DataStreamName("logs", "telemetry-logging", "demo");
+            opts.BootstrapMethod = BootstrapMethod.Failure;
+            opts.ConfigureChannel = channelOpts =>
+            {
+                channelOpts.BufferOptions = new BufferOptions
+                {
+                    ExportMaxConcurrency = 10
+                };
+            };
+        }, transport =>
+        {
+            transport.Authentication(new BasicAuthentication("elastic", "changeme"));
+        })
+        .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger();
     
-    builder.Services.AddSerilog((hostingContext, loggerConfiguration) =>
-        loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
-
-
+    builder.Services.AddSerilog(Log.Logger);
+    
     var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -50,7 +74,7 @@ try {
 }
 catch(Exception ex)
 {
-    Log.Fatal(ex, "Host terminated unexpectedly");
+    Log.Logger.Error(ex.Message);
 }
 finally
 {
