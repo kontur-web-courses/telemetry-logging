0709831a743b75ffeadf9dc6870327befd7cec00
tacserus <115652334+tacserus@users.noreply.github.com>
2024-11-26 16:03:04 +0500

    telemetry-logging is done

--- Program.cs
+++ Program.cs
@@ -1,25 +1,64 @@
+using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Elasticsearch(new []{ new Uri("http://localhost:9200") }, opts =>
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    opts.DataStream = new DataStreamName("logs", "telemetry-logging", "demo");
+    opts.BootstrapMethod = BootstrapMethod.Failure;
+    opts.ConfigureChannel = channelOpts =>
+    {
+        channelOpts.BufferOptions = new BufferOptions
+        {
+            ExportMaxConcurrency = 10
+        };
+    };
+}, transport =>
+    {
+        transport.Authentication(new BasicAuthentication("elastic", "changeme"));
+    })
+    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+try
+{
+    builder.Services.AddRazorPages();
+
+    builder.Host.UseSerilog();
+
+    builder.Services.AddSerilog((hostingContext, loggerConfiguration) =>
+        loggerConfiguration.ReadFrom.Configuration(hostingContext.GetService<HostBuilderContext>().Configuration));
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Logger.Fatal($"Fatal: {e.Message}");
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
