48a20c2d9a479884dea5b44a3e6111561a036bd3
Konstantin Kolesnikov <kolesnikov.kons@gmail.com>
2025-11-10 15:48:44 +0500

    feat: 1.1 - 1.5

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .ReadFrom.Configuration(ctx.Configuration)
+    .Enrich.FromLogContext()
+    .WriteTo.Console());
 
 // Add services to the container.
 builder.Services.AddRazorPages();
