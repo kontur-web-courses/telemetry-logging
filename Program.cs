1c33e291c25efb1b99e4949a47d0b9bf79fab160
kitekatya <prostopostya221@yandex.ru>
2025-11-11 16:16:47 +0500

    3

--- Program.cs
+++ Program.cs
@@ -1,19 +1,45 @@
+using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
 using Serilog;
+
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
     .WriteTo.File(".logs/start-host-log-.txt", rollingInterval: RollingInterval.Day)
-    .CreateLogger();
+    .CreateBootstrapLogger();
 
 try
 {
     var builder = WebApplication.CreateBuilder(args);
+    
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+        .ReadFrom.Configuration(builder.Configuration)
+        .WriteTo.Console()
+        .WriteTo.File(".logs/start-host-log-.txt", rollingInterval: RollingInterval.Day)
+        .WriteTo.Elasticsearch(new [] { new Uri("http://localhost:9200" )}, opts =>
+        {
+            opts.DataStream = new DataStreamName("logs", "console-example", "demo");
+            opts.BootstrapMethod = BootstrapMethod.Failure;
+            opts.ConfigureChannel = channelOpts =>
+            {
+                channelOpts.BufferOptions = new BufferOptions 
+                { 
+                    ExportMaxConcurrency  = 10 
+                };
+            };
+        })
+        .CreateLogger();
 
     builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
         loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration));
 
     // Add services to the container.
     builder.Services.AddRazorPages();
+    builder.Services.AddSerilog(Log.Logger);
 
 
     var app = builder.Build();
