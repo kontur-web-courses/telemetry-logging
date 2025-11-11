c7371533904c6610af9aa5bbb2dd3755263a1dfb
OK <143250301+OK219@users.noreply.github.com>
2025-11-11 16:13:42 +0500

    234

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,9 +17,27 @@ try
 
     builder.Host.UseSerilog();
 
-    builder.Services.AddSerilog((hostingContext, loggerConfiguration) =>
-        loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
-
+    builder.Services.AddSerilog((_, lc) => lc.Enrich.FromLogContext()
+        .WriteTo.Elasticsearch([new Uri("http://localhost:9200")], opts =>
+        {
+            opts.DataStream = new DataStreamName("logs", "telemetry-loggin", "demo");
+            opts.BootstrapMethod = BootstrapMethod.Failure;
+            opts.ConfigureChannel = channelOpts =>
+            {
+                channelOpts.BufferOptions = new BufferOptions
+                {
+                    ExportMaxConcurrency = 10
+                };
+            };
+        }, transport =>
+        {
+            transport.Authentication(new BasicAuthentication("elastic", "changeme")); // Basic Auth
+            // transport.Authentication(new ApiKey(base64EncodedApiKey)); // ApiKey
+            transport.OnRequestCompleted(d => Console.WriteLine($"es-req: {d.DebugInformation}"));
+        })
+        .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+        .ReadFrom.Configuration(builder.Configuration));
+    //builder.Services.AddSerilog(Log.Logger);
     var app = builder.Build();
 
 // Configure the HTTP request pipeline.
