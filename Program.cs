5299f2c43a35faba7b9f1f006d5cda7c2879eb0a
ArtemToporkov <toporkovartem259@gmail.com>
2025-11-11 15:16:23 +0500

    log app starting failure

--- Program.cs
+++ Program.cs
@@ -2,34 +2,42 @@ using Serilog;
 
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
+    .WriteTo.File(".logs/start-host-log-.txt", rollingInterval: RollingInterval.Day)
     .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+    builder.Services.AddRazorPages();
 
-builder.Host.UseSerilog();
-builder.Services.AddSerilog((_, loggerConfiguration) =>
-    loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
+    builder.Host.UseSerilog();
+    builder.Services.AddSerilog((_, loggerConfiguration) =>
+        loggerConfiguration.ReadFrom.Configuration(builder.Configuration));
 
-var app = builder.Build();
+    var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseSerilogRequestLogging();
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapRazorPages();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseSerilogRequestLogging();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal("{exMessage}", ex.Message);
+}
