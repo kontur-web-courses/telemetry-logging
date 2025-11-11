c06b44ccc50f7d618f1640a633b1840fda03584e
artemchick35 <artemryzhkov2004@gmail.com>
2025-11-11 15:45:07 +0500

    add logging

--- Program.cs
+++ Program.cs
@@ -1,4 +1,42 @@
+using Elastic.Channels;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/start-host-log-.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// builder.Services.AddSerilog((hostingContext, loggerConfiguration) =>
+//     loggerConfiguration.ReadFrom.Configuration(hostingContext.GetRequiredService<IConfiguration>()));
+builder.Services.AddSerilog((_, lc) => lc.Enrich.FromLogContext()
+    .WriteTo.Elasticsearch([new Uri("http://localhost:9200")], opts =>
+    {
+        opts.DataStream = new DataStreamName("logs", "telemetry-loggin", "demo");
+        opts.BootstrapMethod = BootstrapMethod.Failure;
+        opts.ConfigureChannel = channelOpts =>
+        {
+            channelOpts.BufferOptions = new BufferOptions
+            {
+                ExportMaxConcurrency = 10
+            };
+        };
+    }, transport =>
+    {
+        transport.Authentication(new BasicAuthentication("elastic", "changeme")); // Basic Auth
+        // transport.Authentication(new ApiKey(base64EncodedApiKey)); // ApiKey
+        transport.OnRequestCompleted(d => Console.WriteLine($"es-req: {d.DebugInformation}"));
+    })
+    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+    .ReadFrom.Configuration(builder.Configuration));
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -15,6 +53,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseSerilogRequestLogging();
 
 app.UseRouting();
 
