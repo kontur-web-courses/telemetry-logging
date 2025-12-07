99eed63092ffc24d24cdd8ba4c153c7321943ca6
Михаил Владимиров <mikhail.vladimirov@urfu.me>
2025-12-08 00:01:19 +0500

    feat: task

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,6 +14,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        var myName = "Temniy Prince";
+        _logger.LogInformation("Sample log. My name is {MyName}", myName);
     }
 }
