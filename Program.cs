fe0eae08a27d6b548fb945f22a14bab749dfbcdc
Daniil Khlyzov <dankhlyzov@gmail.com>
2025-12-15 19:32:22 +0500

    Хлызов Даниил ФТ-401

--- Program.cs
+++ Program.cs
@@ -1,25 +1,69 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.WriteTo.File(".logs/start-host-log-.txt", rollingInterval: RollingInterval.Day)
+	.CreateLogger();
 
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+try
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	var builder = WebApplication.CreateBuilder(args);
+
+	// Configure Serilog with Elasticsearch and other sinks
+	Log.Logger = new LoggerConfiguration()
+		.Enrich.FromLogContext()
+		.WriteTo.Elasticsearch([new Uri("http://localhost:9200")], opts =>
+		{
+			opts.DataStream = new DataStreamName("logs", "telemetry-loggin", "demo");
+			opts.BootstrapMethod = BootstrapMethod.Failure;
+		}, transport =>
+		{
+			transport.Authentication(new BasicAuthentication("elastic", "changeme")); // Basic Auth
+			// transport.Authentication(new ApiKey(base64EncodedApiKey)); // ApiKey
+			transport.OnRequestCompleted(d => Console.WriteLine($"es-req: {d.DebugInformation}"));
+		})
+		.Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
+		.ReadFrom.Configuration(builder.Configuration)
+		.CreateLogger();
+
+	builder.Host.UseSerilog();
+	builder.Services.AddSerilog(Log.Logger);
+
+	// Add services to the container.
+	builder.Services.AddRazorPages();
+
+	var app = builder.Build();
+
+	// Configure the HTTP request pipeline.
+	if (!app.Environment.IsDevelopment())
+	{
+		app.UseExceptionHandler("/Error");
+		// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+		app.UseHsts();
+	}
+
+	app.UseHttpsRedirection();
+	app.UseStaticFiles();
+	app.UseSerilogRequestLogging();
+
+	app.UseRouting();
+
+	app.UseAuthorization();
+
+	app.MapRazorPages();
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
+catch (Exception ex)
+{
+	Log.Fatal(ex, "Host terminated unexpectedly");
+	throw;
+}
+finally
+{
+	Log.CloseAndFlush();
+}
