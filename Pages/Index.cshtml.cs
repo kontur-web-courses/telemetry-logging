5fd00d568d4a8db4ead77c626a121e933ba3e95f
g.host <73241824+MrPr0per@users.noreply.github.com>
2025-12-17 17:59:54 +0500

    4

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -14,6 +14,7 @@ public class IndexModel : PageModel
 
     public void OnGet()
     {
-
+        var myName = "my_name_123";
+        _logger.LogInformation("Sample log. My name is {MyName}", myName);
     }
 }
